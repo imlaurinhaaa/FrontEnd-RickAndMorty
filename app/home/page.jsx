49db4b844ce170d04074cc62ebc14f88440535d5
Laura Violla <lauraviolla.tds1.senai@gmail.com>
2025-04-17 11:39:35 -0300

    feat: add filter and toastify

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,32 +1,82 @@
 "use client";
+
+import { useEffect, useState } from "react";
 import axios from "axios";
-import { useState, useEffect } from "react";
-import CharacterCard from "../../components/CharacterCard";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import styles from "./Home.module.css";
+import CharacterCard from "../../components/CharacterCard";
 
 export default function Home() {
+    const [search, setSearch] = useState("");
     const [characters, setCharacters] = useState([]);
+    const [notFound, setNotFound] = useState(false);
+
+
+    const fetchCharacters = async (name = "") => {
+        setNotFound(false);
+        try {
+            const { data } = await axios.get(`https://rickandmortyapi.com/api/character/?name=${name}`);
+            setCharacters(data.results);
+        } catch {
+            setNotFound(true);
+            setCharacters([]);
+        }
+    };
 
     useEffect(() => {
-        axios.get("https://rickandmortyapi.com/api/character/")
-            .then(response => {
-                setCharacters(response.data.results);
-            })
-            .catch(error => {
-                console.log("Erro ao buscar os personagens", error);
-            });
+        fetchCharacters();
     }, []);
+
+    const handleCardClick = (name) => {
+        toast.info(`Você clicou no personagem: ${name}`, {});
+    };
+
+    const handleButtonClick = (message) => {
+        toast.info(message, {
+            position: "top-right",
+        });
+    };
 
     console.log(characters);
 
-    return(
+    return (
         <div className={styles.container}>
-            <h1 className={styles.title}>Rick and Morty | Personagens 🧪🚀</h1>
-            <div className={styles.grid}>
-            {characters.map((char) => (
-                <CharacterCard key={char.id} character={char} />
-            ))}
+            <ToastContainer position="top-right" autoClose={7500} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="light" />
+            <h1 className={styles.title}>Personagens | Rick and Morty 🚀🧪</h1>
+            <div className={styles.search}>
+
+                <input
+                    className={styles.searchbar}
+                    type="text"
+                    placeholder="Pesquise por um personagem"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)} />
+
+                <button
+                    className={styles.buttonSearch}
+                    onClick={() => {  handleButtonClick("Você buscou por:" + search); fetchCharacters(search.trim()) }}>Buscar</button>
+
+                <button
+                    className={styles.buttonReset}
+                    onClick={() => {
+                        handleButtonClick("Você resetou sua busca!");
+                        setSearch("");
+                        fetchCharacters();
+                    }}>Resetar</button>
             </div>
+
+            {notFound && (
+                <h1 className={styles.notFound}>Nenhum personagem identificado. Tente novamente.</h1>
+            )}
+
+            <div className={styles.grid}>
+                {characters.map((char) => (
+                    <CharacterCard key={char.id} character={char} onClick={() => handleCardClick(char.name)} />
+                ))}
+            </div>
+
         </div>
     );
 }
