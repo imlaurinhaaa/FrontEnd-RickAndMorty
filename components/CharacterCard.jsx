cdd805f709add3c3630ec20259f0ada115c34fb3
Laura Violla <lauraviolla.tds1.senai@gmail.com>
2025-04-24 11:28:22 -0300

    update image

--- components/CharacterCard.jsx
+++ components/CharacterCard.jsx
@@ -1,21 +1,24 @@
 import styles from "../styles/CharacterCard.module.css";
+import Image from "next/image";
 
 export default function CharacterCard({ character, onClick }) {
-    return(
+    return (
         <div className={styles.card} onClick={onClick}>
-            <img 
-            src={character.image}
-            alt={character.name}
-            className={styles.avatar} 
+            <Image
+                src={character.image}
+                alt={character.name}
+                width={225}
+                height={225}
+                className={styles.avatar}
             />
 
             <div className={styles.info}>
-            <h3 className={styles.title}>{character.name}</h3>
-            <p className={styles.text}>Status: {character.status}</p>
-            <p className={styles.text}>Specie: {character.species}</p>
-            <p className={styles.text}>Type: {character.type || "Null"}</p>
-            <p className={styles.text}>Gender: {character.gender}</p>
-        </div>
+                <h3 className={styles.title}>{character.name}</h3>
+                <p className={styles.text}>Status: {character.status}</p>
+                <p className={styles.text}>Specie: {character.species}</p>
+                <p className={styles.text}>Type: {character.type || "Null"}</p>
+                <p className={styles.text}>Gender: {character.gender}</p>
+            </div>
         </div>
     );
 }
